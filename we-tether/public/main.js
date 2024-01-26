fabe0eb8048c33abe6dbd42fe11c17ea3632e15f
Jack Kill <jackkillschool@gmail.com>
2024-01-25 18:54:39 -0600

    Initialized project
    Got React working with electron, running `yarn electron:serve` starts the electron app

---
+++ we-tether/public/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+// Basic webapp startup functionality
+
+
+function createWindow() {
+    // Create the browser window
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+        }
+    })
+
+    win.loadURL('http://localhost:3000');
+}
+
+app.on('ready', createWindow);
+
+// Additional listeners specifically for MacOS
+
+app.on('window-all-closed', function() {
+    // On MacOS, applications usually don't fully close unless user hits command+Q
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', function() {
+    // On MacOS, windows get re-created in the app when the dock icon is clicked and there are no other windows open
+    if(BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+})
