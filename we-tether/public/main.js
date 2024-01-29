1ed064368c5a21bbb872e4c7feaa4796e4af11bb
Jack Kill <jackkillschool@gmail.com>
2024-01-29 13:59:20 -0600

    Removed electron-is-dev implementation
    Package seemed to be deprecated and breaking things. Not sure why it was useful, if I discover missing functionality I'll try to find an alternative.

--- we-tether/public/main.js
+++ we-tether/public/main.js
@@ -5,7 +5,6 @@ Created: 2024-01-25
 */
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const isDev = require('electron-is-dev');
 
 function createWindow() {
     // Create the browser window
@@ -17,11 +16,7 @@ function createWindow() {
         }
     })
 
-    win.loadURL(
-        isDev
-            ? 'http://localhost:3000'
-            : `file://${path.join(__dirname, '../build/index.html')}`
-    );
+    win.loadURL('http://localhost:3000');
 }
 
 app.on('ready', createWindow);
